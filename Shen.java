33f1cb7c93e1af5b21a99343deca0bee504c9f2c
shen-iya <sheniya.502251@sxcce.edu.in>
2025-01-30 14:21:55 +0530

    Add files via upload

---
+++ Shen.java
@@ -0,0 +1,17 @@
+package com.mycompany.shen;
+
+import java.util.Scanner; 
+class Shen {
+  public static void main(String[] args) {
+    int x, y, sum;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Type a number:");
+    x = sc.nextInt();
+
+    System.out.println("Type another number:");
+    y = sc.nextInt(); 
+
+    sum = x + y;  
+    System.out.println("Sum is: " + sum); 
+  }
+} 
